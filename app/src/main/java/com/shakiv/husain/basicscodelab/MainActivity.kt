df961de99c583be29c19334c6fb8792552f4dab3
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-05-08 19:47:58 +0530

    ✅ Create a performant lazy list

--- app/src/main/java/com/shakiv/husain/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/shakiv/husain/basicscodelab/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -60,16 +62,20 @@ fun MyAppReview() {
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000){"$it"}
 ) {
+
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background
     ) {
 
-        Column(modifier = Modifier.padding(4.dp)) {
-            for (name in names) {
-                Greeting("$name")
+        LazyColumn(modifier = Modifier.padding(vertical = 4.dp)){
+
+            items(items = names){name->
+
+                Greeting(name = name)
+
             }
         }
     }
